2b2018a909888985e4be8466cd0cc5c59fc04b5e
raemib333 <121309928+raemib333@users.noreply.github.com>
2023-07-22 16:44:34 +0200

    working version 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 import requests
 import plotly.express as px
+import datetime
 
 st.set_page_config(page_title="Data you need to see",
                    layout="wide")
@@ -25,7 +26,7 @@ def get_data(start, end, lat, long):
     return response_df
 
 # initialize all variable
-start = '2023-07-02'
+start = '1991-01-01'
 end = '2023-07-15'
 lat = '47.22'
 long = '8.33'
@@ -39,6 +40,22 @@ data["t_max"] = raw_data["daily"]["temperature_2m_max"]
 data["t_min"] = raw_data["daily"]["temperature_2m_min"]
 data["t_avg"] = (data["t_max"] + data["t_min"]) / 2
 
+# calculate yearly average
+# calculate the year for each date
+data["date"] = pd.to_datetime(data["date"])
+data["year"] = data["date"].dt.year
+
+# calculate the average temperature per year
+year_averages = data.groupby("year")["t_avg"].mean()
+
+# add the average yearly temperature to all entries in the data variable
+data["t_avg_year"] = data["year"].apply(lambda year: year_averages[year])
+
+# print the year averages
+st.write(year_averages)
+
+st.line_chart(data = data, x="date", y=["t_avg", "t_avg_year"])
+
 #print(data)
 #fig = px.line(data, x="date", y="t_avg")
 #fig.show()
@@ -46,4 +63,4 @@ data["t_avg"] = (data["t_max"] + data["t_min"]) / 2
 
 st.dataframe(data)
 
-st.line_chart(data = data, x="date", y="t_avg")
+st.line_chart(data = data, x="date", y="t_avg_year")
